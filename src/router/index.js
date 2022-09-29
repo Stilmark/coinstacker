78e59f4fb8c6c175238ddf0d55f403da72c7ae24
Christian Lund <clund@stilmark.com>
2022-09-29 19:23:56 +0200

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      meta: {
+        title: 'Start'
+      },
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = (to.meta.title ?? 'Coinstacker') + (process.env.NODE_ENV === 'development' ? ' ['+process.env.NODE_ENV+']':'');
+  next();
+});
+
+export default router
